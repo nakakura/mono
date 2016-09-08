f85a3b4873f9b864dd50959eab33472b15dff75f
Toshiya Nakakura <nakakura@Toshiyas-MacBook-Pro.local>
2016-09-08 18:01:19 +0900

    input textをキレイにsplit

--- scripts/mono.js
+++ scripts/mono.js
@@ -1,11 +1,23 @@
 var Mono = require('../dist/main').Main.Mono;
+var _ = require('lodash');
+
+function format(text){
+  var args = text.match(/(".*?”|‘.*?'|'.*?'|".*?"|“.*?”|“.*?"|\S+)/g);
+  return _.map(args, function(arg){
+    return arg.replace(/'|“|‘|”|\"/g, "");
+  });
+}
 
 module.exports = function (robot) {
   robot.respond(new RegExp("add.*", "i"), function (msg) {
     var user = msg.message.user.name;
-    var args = msg.match[0];
-    console.log(user);
-    console.log(args);
+    var args = format(msg.match[0].trim());
+
+    if(args.length < 2) return;
+    var command = args[1];
+    var params = args.slice(2, args.length);
+    console.log(command);
+    console.log(params);
   });
 };
 
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ export class Mono{
         console.log("にほんごA;");
       });
     });
+
   }
 
 
